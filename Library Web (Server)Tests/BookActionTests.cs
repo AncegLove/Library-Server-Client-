d16a87835bf9963d89c76291a1852893f33469ef
AncegLove <ancegloves3@gmail.com>
2025-01-22 14:02:39 +0300

    Добавлена библиотека и класс для тестирования

---
+++ Library Web (Server)Tests/BookActionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library__Web_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.Extensions.Logging;
+
+namespace Library__Web_.Tests
+{
+    [TestClass()]
+    public class BookActionTests
+    {
+        [TestMethod()]
+        public void AddBookTest()
+        {
+            //arrange
+            var mockLocker = new Mock<ILogger>();
+            var mockRepository = new Mock<IBookRepository>();
+
+            
+            //act
+
+            //assert
+        }
+    }
+}
---
+++ Library Web (Server)Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Library_Web__Server_Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
