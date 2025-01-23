0aa24041415df9a9e8ef00ee0d5eebe2b5ece79c
AncegLove <ancegloves3@gmail.com>
2025-01-23 07:50:53 +0300

    Добавлен файл и класс MockLogger.cs

---
+++ Library Web (Server)Tests/MockLogger.cs
@@ -0,0 +1,31 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Library_Web__Server_Tests
+{
+    public class MockLogger<T> : ILogger<T>
+    {
+        public List<string> LoggedMeassages { get; } = new List<string>();
+        public IDisposable? BeginScope<TState>(TState state) => null;
+        public bool IsEnabled(LogLevel level) => true;
+
+        public void Log<TState>(LoggerLevel level, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formater)
+        {
+            if (formater != null)
+            {
+                LoggedMeassages.Add(formater(state, exception));
+            }
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
