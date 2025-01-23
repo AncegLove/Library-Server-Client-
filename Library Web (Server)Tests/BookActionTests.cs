5ca471565532a3141db95e4400b5eb14df310727
AncegLove <ancegloves3@gmail.com>
2025-01-23 04:35:22 +0300

    Добавлен метод UpdateBookTest()

--- Library Web (Server)Tests/BookActionTests.cs
+++ Library Web (Server)Tests/BookActionTests.cs
@@ -34,9 +34,34 @@ namespace Library__Web_.Tests
             bookAction.AddBook(newBook);
 
             // Assert
+            mockLogger.Verify(logger => logger.LogInformation("Add new book", Times.Once));
             Assert.AreEqual(2, newBook.Id);
             Assert.IsTrue(newBook.IsAviable);
             CollectionAssert.Contains(existingBooks, newBook);
+        }
+        [TestMethod()]
+        public void UpdateBookTest()
+        {
+            //arrange
+            var mockLogger = new Mock<ILogger<BookAction>>();
+            var mockRepository = new Mock<IBookRepository>();
+            var existingBooks = new List<Book>
+            {
+                new Book(1, "Title", "Author", 1995, new string[] {"Fiction"}, true)
+            };
+            mockRepository.Setup(repo => repo.GetAll()).Returns(existingBooks);
+            var bookAction = new BookAction(mockRepository.Object, mockLogger.Object);
+            var updateBook = new Book(-1, "updateTitle", "updateAuthor", 5333, new string[] { "updateFictions" }, false);
+            int bookId = 1;
+            //act
+            bookAction.UpdateBook(bookId, updateBook);
+            //Assert
+            mockLogger.Verify(logger => logger.LogInformation($"Update book: {bookId}", Times.Once));
+            StringAssert.Equals(updateBook.Title, mockRepository.Object.GetAll().FirstOrDefault(x => x.Id == bookId).Title);
+            StringAssert.Equals(updateBook.Author, mockRepository.Object.GetAll().FirstOrDefault(x => x.Id == bookId).Author);
+            Assert.AreEqual(updateBook.Year, mockRepository.Object.GetAll().FirstOrDefault(x => x.Id == bookId).Year);
+            CollectionAssert.AreEqual(updateBook.Genres, mockRepository.Object.GetAll().FirstOrDefault(x => x.Id == bookId).Genres);
+
         }
     }
 }
